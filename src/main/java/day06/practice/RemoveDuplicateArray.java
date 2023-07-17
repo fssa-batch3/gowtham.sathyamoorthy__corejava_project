bb82e901163dbd4d36ffd13ef5e4b109bc49523b
Gowtham11234 <118426147+Gowtham11234@users.noreply.github.com>
2023-07-17 22:04:11 +0530

    day06_update

---
+++ src/main/java/day06/practice/RemoveDuplicateArray.java
@@ -0,0 +1,27 @@
+package day06.practice;
+
+import java.util.ArrayList;
+
+public class RemoveDuplicateArray{
+    public static void main(String[] args) {
+        // Create an ArrayList with duplicate elements
+        ArrayList<String> cityList = new ArrayList<String>();
+        cityList.add("Chennai");
+        cityList.add("Chennai");
+        cityList.add("Mumbai");
+        cityList.add("Mumbai");
+        cityList.add("Delhi");
+        cityList.add("Lucknow");
+
+        ArrayList<String> duplicateList = new ArrayList<String>();
+        for (String list : cityList) {
+            if (!duplicateList.contains(list)) {
+            	duplicateList.add(list);
+            }
+        }
+
+        for (String lists : duplicateList) {
+            System.out.println(lists);
+        }
+    }
+}
---
+++ src/main/java/day06/solved/StringArray.java
@@ -0,0 +1,21 @@
+//package day06.solved;
+//import java.util.Arrays;
+//public class StringArray {
+//
+//	public static void main(String[] args) {
+//
+//		// How its stored in an normal array
+//		String[] cityArr = new String[4];
+//		cityArr[0] = "Chennai";
+//		cityArr[1] = "Bangalore";
+//		cityArr[2] = "Mumbai";
+//
+//		// Display the city names
+//		for (String cityName : cityArr) {
+//			System.out.println(cityName);
+//		}
+//		
+//		 int cityArr.set(3);
+//		System.out.println("Delhi");
+//	}
+//}
