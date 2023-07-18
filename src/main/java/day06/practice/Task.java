facd13e3ec798e5042572a6c9602fc6e5332ed3d
Gowtham11234 <118426147+Gowtham11234@users.noreply.github.com>
2023-07-18 16:19:25 +0530

    day07 update

--- src/main/java/day06/practice/Task.java
+++ src/main/java/day06/practice/Task.java
@@ -1,37 +1,49 @@
 package day06.practice;
 import java.util.ArrayList;
+class addTask {
+
+	private int taskId;
+	String taskName;
+	private String description;
+
+	// Constructor
+	public addTask(int taskId, String taskName, String description) {
+		this.taskId = taskId;
+		this.taskName = taskName;
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "taskID: " + taskId + ", taskName: " + taskName + ", description: " + description;
+	}
+}
 public class Task {
+	public boolean findTaskByName(String name, ArrayList<addTask> taskList) {
+		boolean check = false;
 
-		public String taskName;
+		for (addTask value : taskList) {
+			if (value.taskName.equals(name)) {
+				check = true;
+				break;
+			}
 
-		public int priority;
-
-		public static void main(String[] args) {
-
-			ArrayList<Task> taskList = new ArrayList<>();
-
-			Task task1 = new Task();
-			task1.taskName = "Task 1";
-			task1.priority = 1;
-			taskList.add(task1);
-
-			Task task2 = new Task();
-			task2.taskName = "Task 2";
-			task2.priority = 2;
-			taskList.add(task2);
-
-			Task task3 = new Task();
-			task3.taskName = "Task 3";
-			task3.priority = 3;
-			taskList.add(task3);
-			
-			Task task4 = new Task();
-			task3.taskName = "Task 4";
-			task3.priority = 4;
-			taskList.add(task3);
-			
-			
-			System.out.println(taskList);
 		}
+		return check;
+	}
+
+	public static void main(String[] args) {
+
+		ArrayList<addTask> taskList = new ArrayList<>();
+
+		taskList.add(new addTask(1, "Task 1", "Complete practice"));
+		taskList.add(new addTask(2, "Task 2", "Do FacePrep Sums."));
+		taskList.add(new addTask(3, "Task 3", "Prepare for the CPR."));
+
+		for (addTask task : taskList) {
+			System.out.println(task);
+		}
+
+	}
 	}
 
