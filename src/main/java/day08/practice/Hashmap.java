23e83db783e4be7bca447e1ffd406146d39633ee
Gowtham11234 <118426147+Gowtham11234@users.noreply.github.com>
2023-07-20 19:40:21 +0530

    day09

--- src/main/java/day08/practice/Hashmap.java
+++ src/main/java/day08/practice/Hashmap.java
@@ -1,29 +1,33 @@
 package day08.practice;
+
 import java.util.HashMap;
 import java.util.Map;
+
 //import java.util.Scanner;
 public class Hashmap {
 	public static void main(String[] args) {
 		// MAP IS TO COUNT
-		        Map<String, Integer> nameCountMap = new HashMap<>();
-		      // ASK THE INPUT FROM THE USER IN THE STRING WITH NEXTLINE BECAUSE HOW MANY NAME GIVE WE CAN SPLIT IT USING ","
-		       		       
-		        String input = "Ram,Ram,Spider,spider,Superman,spider,hey,hello,hey";
-		        // AFTER THAT STORE IT IN THE ARRAY TO RUN THE LOOP FOR THE COUNT
-		        String[] names = input.split(",\\s*");
-		       
-		        //NOW WE CREATE THE FOR EACH LOOP
-		       //NAMECOUNT FROM THE MAP METHOD IT CAN COUNT
-		      // AND TRIM() TO AVOID SPACE AND TOLOWERCASE IS TO CONVERT ALL INTO LOWERCASE AND COUNT IT
-		       
-		        for (String name : names) {
-		            nameCountMap.put(name.toLowerCase().trim(), nameCountMap.getOrDefault(name.toLowerCase().trim(), 0) + 1);
-		        }
+		Map<String, Integer> nameCountMap = new HashMap<>();
+		// ASK THE INPUT FROM THE USER IN THE STRING WITH NEXTLINE BECAUSE HOW MANY NAME
+		// GIVE WE CAN SPLIT IT USING ","
+
+		String input = "Ram,Ram,Spider,spider,Superman,spider,hey,hello,hey";
+		// AFTER THAT STORE IT IN THE ARRAY TO RUN THE LOOP FOR THE COUNT
+		String[] names = input.split(",\\s*");
+
+		// NOW WE CREATE THE FOR EACH LOOP
+		// NAMECOUNT FROM THE MAP METHOD IT CAN COUNT
+		// AND TRIM() TO AVOID SPACE AND TOLOWERCASE IS TO CONVERT ALL INTO LOWERCASE
+		// AND COUNT IT
+
+		for (String name : names) {
+			nameCountMap.put(name.toLowerCase().trim(), nameCountMap.getOrDefault(name.toLowerCase().trim(), 0) + 1);
+		}
 		// WHEN PRINT THAT AS USUALL WHAT WE GOT FROM THE SCANNER
-		        for (Map.Entry<String, Integer> entry : nameCountMap.entrySet()) {
-		            String name = entry.getKey();
-		            int count = entry.getValue();
-		            System.out.println(name + ": " + count);
-		        }
-    	 }
+		for (Map.Entry<String, Integer> entry : nameCountMap.entrySet()) {
+			String name = entry.getKey();
+			int count = entry.getValue();
+			System.out.println(name + ": " + count);
+		}
+	}
 }
---
+++ src/main/java/day09/practice/Sorting_integer.java
@@ -0,0 +1,20 @@
+package day09.practice;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Sorting_integer {
+	public static void main(String[] args) {
+		ArrayList<Integer> Numbers = new ArrayList<>();
+		Numbers.add(8);
+		Numbers.add(9);
+		Numbers.add(45);
+		Numbers.add(12);
+		Numbers.add(1);
+		
+
+		System.out.println("Before Sort:" + Numbers);
+		Collections.sort(Numbers);
+		System.out.println("After Sort:" + Numbers);
+		Collections.reverse(Numbers);
+		System.out.println("After reverse Sort:" + Numbers);
+	}
+}
